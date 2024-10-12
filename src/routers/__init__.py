bcd36736baf4df657c63b35b9975cba49265e5d7
SabaXdev <sabakatamadze@gmail.com>
2024-10-12 16:31:58 +0400

    Delete routers

--- src/routers/__init__.py
+++
@@ -1,8 +0,0 @@
-# src/routers/__init__.py
-
-from src.routers.molecules import router as molecules_router
-from src.routers.search import router as search_router
-from src.routers.upload import router as upload_router
-from src.routers.utils import router as utils_router
-
-__all__ = ["molecules_router", "search_router", "upload_router", "utils_router"]
